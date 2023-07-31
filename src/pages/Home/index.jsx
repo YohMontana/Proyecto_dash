82c8489f993cff7ff80c891bd3f7819764ef4794
Jhancarlo Esteban <estebanjcarlo@gmail.com>
2023-07-31 10:00:11 -0500

    import Link pues

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
 
 import {
   
@@ -116,7 +118,7 @@ const Home = () => {
             </div>
             {/* cards */}
             <Link
-              to="#"
+              to={"/"}
               className="bg-white rounded-2xl p-8 flex flex-col md:flex-row gap-8 w-full drop-shadow-lg border-2 border-transparent hover:border-purple-400 transition-all mb-4"
             >
               {/* Icon */}
