142a36c28465d4ddc95fab7e960638b8147be586
Jhancarlo Esteban <estebanjcarlo@gmail.com>
2023-08-01 08:40:56 -0500

    correo/otro corregido

--- src/components/Enviar.jsx
+++ src/components/Enviar.jsx
@@ -147,7 +147,8 @@ return (
           type="email"
           id="correo"
           name="correo"
-          value={emailData.para}          
+          value={emailData.para}
+          onChange={(e) => setEmailData({ ...emailData, para: e.target.value })}          
           className="bg-gray-50 border w-[50%] border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder="otro_correo@une.edu.pe"
         />
